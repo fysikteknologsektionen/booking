a7840751c61a1deb7719a50cad8557b6b95556e5
Eric <32720134+ECarlsson@users.noreply.github.com>
2022-08-28 16:51:41 +0000

    Add logging

---
+++ lib/logger.ts
@@ -0,0 +1,13 @@
+import { createLogger, format, transports } from "winston";
+
+const logger = createLogger({
+  format: format.combine(format.timestamp(), format.json()),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: "logs/error.log", level: "error" }),
+    new transports.File({ filename: "logs/activity.log", level: "info" }),
+    new transports.File({ filename: "logs/combined.log" }),
+  ],
+});
+
+export default logger;
